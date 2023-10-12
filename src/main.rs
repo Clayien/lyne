8918739dcefc09ebfac7f8402378e75798f90b21
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2023-10-13 02:07:27 +0530

    feat can activate envs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ pub mod fzf;
 pub mod parser;
 
 use std::fs;
+use std::path::Path;
+use std::process::{Command, Stdio};
 
 use clap::Parser;
 
@@ -24,14 +26,82 @@ fn main() {
     // for _ in 0..args.count {
     //     println!("Hello {}!", args.name)
     // }
-    let config = parser::parse();
+    let mut config = parser::Config::new();
+    config.parse_config();
 
-    let project_dirs = fs::read_dir(config.main.project_dir).unwrap();
     let mut options: Vec<String> = Vec::new();
-    for project_dir in project_dirs {
-        options.push(project_dir.unwrap().path().display().to_string());
+    let project_dirs = fs::read_dir(config.project_dir.unwrap()).unwrap();
+    for dir in project_dirs {
+        if config.language_seperated.unwrap() {
+            for sub_dir in fs::read_dir(dir.unwrap().path().display().to_string()).unwrap() {
+                options.push(sub_dir.unwrap().path().display().to_string());
+            }
+        } else {
+            options.push(dir.unwrap().path().display().to_string());
+        }
     }
 
-    let output = fzf::choose(options);
-    print!("{output}");
+    let mut shell_output = Vec::new();
+    let chosen_dir = fzf::choose(options);
+    let language_config_path = Path::new(&chosen_dir)
+        .join(".clayien")
+        .join("lyne")
+        .join("config.toml");
+
+    shell_output.push(format!("cd {chosen_dir}"));
+    if language_config_path.exists() {
+        let mut language_config = parser::LanguageConfig::new();
+        language_config.parse_config(language_config_path.display().to_string());
+
+        let mut conda_envs = parser::CondaEnvs::new();
+        let conda_info_output = Command::new("conda")
+            .args(["info", "--json"])
+            .output()
+            .unwrap();
+
+        let output_string = if conda_info_output.status.success() {
+            String::from_utf8(conda_info_output.stdout).unwrap()
+        } else {
+            "".to_string()
+        };
+        conda_envs.parse_config(output_string);
+        if conda_envs
+            .envs
+            .unwrap()
+            .iter()
+            .any(|e| e.contains(&language_config.env.as_ref().unwrap().env))
+        {
+            shell_output.push(format!(
+                "conda activate {}",
+                language_config.env.unwrap().env
+            ));
+        } else {
+            let mut conda_create = Command::new("conda")
+                .args([
+                    "create",
+                    format!("--name={}", language_config.env.as_ref().unwrap().env).as_str(),
+                    "python",
+                    "-y",
+                ])
+                .stdout(Stdio::null())
+                .stderr(Stdio::null())
+                .spawn()
+                .expect("Failed to spawn conda create output");
+            if conda_create.wait().unwrap().success() {
+                println!("Conda created successfully");
+
+                shell_output.push(format!(
+                    "conda activate {}",
+                    language_config.env.unwrap().env
+                ));
+                shell_output.push("pip install -r requirements.txt".to_string());
+            } else {
+                println!("Conda creation failed");
+            }
+        }
+    }
+
+    for o in shell_output {
+        print!("{o}; ");
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,26 +1,118 @@
 use serde::Deserialize;
+use serde_json;
 use std::fs;
 use std::path::Path;
 use toml;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Config {
-    pub main: Main,
+    pub project_dir: Option<String>,
+    pub language_seperated: Option<bool>,
 }
 
-#[derive(Deserialize)]
-pub struct Main {
-    pub project_dir: String,
-    pub language_seperated: bool,
-    pub random: Option<bool>,
+#[derive(Deserialize, Debug)]
+pub struct LanguageConfig {
+    pub language: Option<String>,
+    pub env: Option<Env>,
+    pub init: Option<Init>,
 }
 
-fn read_config_file() -> String {
-    let file_path = Path::new("/home/symph/.config/clayien/lyne/config.toml");
+#[derive(Deserialize, Debug)]
+pub struct Env {
+    pub command: String,
+    pub env: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Init {
+    pub command: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CondaEnvs {
+    pub envs: Option<Vec<String>>,
+}
+
+fn read_config_file(path: String) -> String {
+    let file_path = Path::new(&path);
 
     fs::read_to_string(file_path).unwrap()
 }
 
-pub fn parse() -> Config {
-    toml::from_str(read_config_file().as_str()).unwrap()
+impl Config {
+    pub fn new() -> Self {
+        Self {
+            project_dir: Some("/home/symph/Documents".to_string()),
+            language_seperated: Some(true),
+        }
+    }
+
+    pub fn parse_config(&mut self) {
+        if let Ok(user_values) = toml::from_str::<Config>(&read_config_file(
+            "/home/symph/.config/clayien/lyne/config.toml".to_string(),
+        )) {
+            if let Some(project_dir) = user_values.project_dir {
+                self.project_dir = Some(project_dir);
+            }
+            if let Some(language_seperated) = user_values.language_seperated {
+                self.language_seperated = Some(language_seperated);
+            }
+        }
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl LanguageConfig {
+    pub fn new() -> Self {
+        Self {
+            language: None,
+            env: None,
+            init: None,
+        }
+    }
+
+    pub fn parse_config(&mut self, path: String) {
+        if let Ok(user_values) = toml::from_str::<LanguageConfig>(&read_config_file(path)) {
+            if let Some(language) = user_values.language {
+                self.language = Some(language);
+            }
+            if let Some(env) = user_values.env {
+                self.env = Some(env);
+            }
+            if let Some(init) = user_values.init {
+                self.init = Some(init);
+            }
+        }
+    }
+}
+
+impl Default for LanguageConfig {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl CondaEnvs {
+    pub fn new() -> Self {
+        Self { envs: None }
+    }
+
+    pub fn parse_config(&mut self, data: String) {
+        if let Ok(info) = serde_json::from_str::<CondaEnvs>(&data) {
+            if let Some(envs) = info.envs {
+                self.envs = Some(envs);
+            }
+        }
+    }
+}
+
+impl Default for CondaEnvs {
+    fn default() -> Self {
+        Self::new()
+    }
 }
